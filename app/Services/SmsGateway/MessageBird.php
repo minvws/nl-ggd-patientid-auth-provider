6f3447c6ea7514973ef1b7f054206f1aab650e65
Joshua Thijssen <jthijssen@noxlogic.nl>
2022-05-04 11:54:01 +0200

    Added messagebird gateway

---
+++ app/Services/SmsGateway/MessageBird.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\SmsGateway;
+
+use MessageBird\Client;
+use MessageBird\Objects\Message;
+
+class MessageBird implements SmsGatewayInterface
+{
+    protected string $apiKey;
+
+    public function __construct(string $apiKey)
+    {
+        $this->apiKey = $apiKey;
+    }
+
+    public function send(string $phoneNr, string $template, array $vars): bool
+    {
+        $client = new Client($this->apiKey);
+
+        $msg = new Message();
+        $msg->originator = 'Alarmallama';
+        $msg->recipients = array($phoneNr);
+        $msg->body = 'Jouw persoonlijke PAP code is ' . $vars['code'];
+
+        try {
+            $client->messages->create($msg);
+        } catch (\Throwable $e) {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ config/messagebird.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'api_key' => env('SMS_GATEWAY_MESSAGEBIRD_API_KEY', ''),
+];
