555165f39cc40e0b3ca82ef7b56332c3e73e8b14
Joshua Thijssen <jthijssen@noxlogic.nl>
2022-05-09 15:45:49 +0200

    temp test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -15,6 +15,7 @@ class ExampleTest extends TestCase
      */
     public function testApplication()
     {
+        $this->assertTrue(true);
 //        $response = $this->get('/');
 //
 //        $response->assertStatus(200);
