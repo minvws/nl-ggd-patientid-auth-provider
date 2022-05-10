791b4bcb5920181c9acce204369f581197c0e5e1
Joshua Thijssen <jthijssen@noxlogic.nl>
2022-05-10 15:16:50 +0200

    ci fix

--- app/View/Components/ConfirmationForm.php
+++ app/View/Components/ConfirmationForm.php
@@ -15,11 +15,14 @@ class ConfirmationForm extends Component
     public function __construct(OidcService $service, string $url)
     {
         $request = request();
-        $this->access_token = $service->fetchTokenFromRequest($request);
+        $this->access_token = (string)$service->fetchTokenFromRequest($request);
 
         $this->url = $url;
     }
 
+    /**
+     * @return \Closure|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Support\Htmlable|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|string
+     */
     public function render()
     {
         return view('components.confirmation-form');
