c6de657ea700fff290394db8e531cb09f7ef3aa4
Anne Jan Brouwer <brouwer@annejan.com>
2022-06-13 12:35:32 +0200

    Default page and bigger key, fixes #57

--- scripts/generate-jwt-key.sh
+++ scripts/generate-jwt-key.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 cd "$(dirname "$0")/../secrets"
-openssl genrsa -out jwt.key 2048
+openssl req -x509 -nodes -newkey rsa:4096 -keyout jwt.key -out cert.pem -sha256 -days 365 -subj '/CN=pap.localdev'
+
