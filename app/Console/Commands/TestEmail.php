c5469fbbefe7ffe0adf844dd91ac6fedd633733a
Anne Jan Brouwer <brouwer@annejan.com>
2022-06-13 15:26:06 +0200

    Test commands for SMS and Mail

---
+++ app/Console/Commands/TestEmail.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+use App\Services\EmailService;
+use Illuminate\Console\Command;
+
+class TestEmail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test:email {email} {code}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send a test email';
+
+    protected EmailService $emailService;
+
+    public function __construct(
+        EmailService $emailService,
+    ) {
+        parent::__construct();
+
+        $this->emailService = $emailService;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        return (int) $this->emailService->send(
+            $this->argument('email'),
+            'template',
+            ['code' => $this->argument('code')]
+        );
+    }
+}
