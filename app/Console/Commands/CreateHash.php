e587fda54b79e15559e14a7a7a937b2ad62ae784
Peter-Paul van Gemerden <mail@ppvg.nl>
2022-06-21 10:48:16 +0200

    chore: add create:hash command (#66)
    * chore: add create:hash command
    
    * Update app/Console/Commands/CreateHash.php
    
    Co-authored-by: Rick Lambrechts <rick.lambrechts@hotmail.com>
    
    * Update app/Console/Commands/CreateHash.php
    
    Co-authored-by: Rick Lambrechts <rick.lambrechts@hotmail.com>
    
    Co-authored-by: Rick Lambrechts <rick.lambrechts@hotmail.com>

---
+++ app/Console/Commands/CreateHash.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+use App\Services\CodeGeneratorService;
+use Illuminate\Console\Command;
+
+class CreateHash extends Command
+{
+    protected CodeGeneratorService $codeGeneratorService;
+
+    protected $signature = 'create:hash {patient_id} {birthdate}';
+    protected $description = 'Create hash based on patient id and birthdate';
+
+    public function __construct(CodeGeneratorService $codeGeneratorService)
+    {
+        parent::__construct();
+        $this->codeGeneratorService = $codeGeneratorService;
+    }
+
+    public function handle(): int
+    {
+        $patient_id = $this->argument("patient_id");
+        $birthdate = $this->argument("birthdate");
+
+        $this->line($this->codeGeneratorService->createHash(
+            is_string($patient_id) ? $patient_id : "",
+            is_string($birthdate) ? $birthdate : "",
+        ));
+
+        return 0;
+    }
+}
