31f3a2a602c41317d24982f52277f5dca624dfff
Peter-Paul van Gemerden <mail@ppvg.nl>
2022-06-28 13:32:00 +0200

    chore: add dummy modes for SmsGateway and EmailGateway

--- app/Providers/GatewayProvider.php
+++ app/Providers/GatewayProvider.php
@@ -5,13 +5,11 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use App\Services\CmsService;
-use App\Services\EmailGateway\Native;
+use App\Services\EmailGateway;
 use App\Services\EmailService;
-use App\Services\InfoRetrievalGateway\Dummy as InfoRetrievalGatewayDummy;
-use App\Services\InfoRetrievalGateway\Yenlo;
+use App\Services\InfoRetrievalGateway;
 use App\Services\InfoRetrievalService;
-use App\Services\SmsGateway\MessageBird;
-use App\Services\SmsGateway\RateLimiter;
+use App\Services\SmsGateway;
 use App\Services\SmsService;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,47 +18,66 @@ class GatewayProvider extends ServiceProvider
     public function boot(): void
     {
         $this->app->singleton(SmsService::class, function () {
-            return new SmsService(
-                new RateLimiter(
-                    new MessageBird(
-                        config('messagebird.api_key'),
-                        config('messagebird.sender'),
-                    ),
-                    config('messagebird.ratelimit')
-                ),
-            );
+            $provider = env('SMS_SERVICE', 'messagebird');
+            switch ($provider) {
+                case 'dummy':
+                    return new SmsService(new SmsGateway\Dummy());
+                case 'messagebird':
+                    return new SmsService(
+                        new SmsGateway\RateLimiter(
+                            new SmsGateway\MessageBird(
+                                config('messagebird.api_key'),
+                                config('messagebird.sender'),
+                            ),
+                            config('messagebird.ratelimit')
+                        ),
+                    );
+                default:
+                    throw new \Exception("Invalid SMS_SERVICE '" . $provider . "'");
+            }
         });
 
         $this->app->singleton(EmailService::class, function () {
-            return new EmailService(new Native());
+            $provider = env('EMAIL_SERVICE', 'native');
+            switch ($provider) {
+                case 'dummy':
+                    return new EmailService(new EmailGateway\Dummy());
+                case 'native':
+                    return new EmailService(new EmailGateway\Native());
+                default:
+                    throw new \Exception("Invalid EMAIL_SERVICE '" . $provider . "'");
+            }
         });
 
-        $this->app->singleton(Yenlo::class, function () {
-            return new Yenlo(
-                new CmsService(
-                    config('cms.cert'),
-                    config('cms.chain'),
-                ),
-                config('yenlo.client_id'),
-                config('yenlo.client_secret'),
-                config('yenlo.token_url'),
-                config('yenlo.userinfo_url'),
+        $this->app->singleton(CmsService::class, function () {
+            return new CmsService(
+                config('cms.cert'),
+                config('cms.chain'),
             );
         });
 
         $this->app->singleton(InfoRetrievalService::class, function () {
-            switch (env('INFORETRIEVAL_SERVICE', 'yenlo')) {
+            $provider = env('INFORETRIEVAL_SERVICE', 'yenlo');
+            switch ($provider) {
                 case 'dummy':
-                    $provider = new InfoRetrievalGatewayDummy(config('codegenerator.hmac_key', ''));
-                    break;
+                    return new InfoRetrievalService(
+                        new InfoRetrievalGateway\Dummy(
+                            config('codegenerator.hmac_key', '')
+                        )
+                    );
                 case 'yenlo':
-                    $provider = $this->app->get(Yenlo::class);
-                    break;
+                    return new InfoRetrievalService(
+                        new InfoRetrievalGateway\Yenlo(
+                            $this->app->get(CmsService::class),
+                            config('yenlo.client_id'),
+                            config('yenlo.client_secret'),
+                            config('yenlo.token_url'),
+                            config('yenlo.userinfo_url'),
+                        )
+                    );
                 default:
-                    throw new \Exception("please provide your info provider through INFORETRIEVAL_SERVICE");
+                    throw new \Exception("Invalid INFORETRIEVAL_SERVICE '" . $provider . "'");
             }
-
-            return new InfoRetrievalService($provider);
         });
     }
 }
