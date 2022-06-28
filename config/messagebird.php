f9197f1f243f194f3fc39603c23494b7eae72a1e
Joshua Thijssen <jthijssen@noxlogic.nl>
2022-06-28 10:54:00 +0200

    Added rate limiter for sms

--- config/messagebird.php
+++ config/messagebird.php
@@ -5,4 +5,5 @@ declare(strict_types=1);
 return [
     'api_key' => env('SMS_GATEWAY_MESSAGEBIRD_API_KEY', ''),
     'sender' => env('SMS_GATEWAY_SENDER', env('APP_NAME', '')),
+    'ratelimit' => (int)env('SMS_GATEWAY_RATELIMIT', 5),
 ];
