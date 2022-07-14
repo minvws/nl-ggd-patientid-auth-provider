3be8736a0037a728a30c61e5e87a011fad026c4b
Peter-Paul van Gemerden <mail@ppvg.nl>
2022-07-14 11:10:16 +0200

    feat: language picker (#119)

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,15 @@
 <header>
     <div>
         <img src="/logo.svg" alt="GGD GHOR" width="84px" height="39.06px">
+        <div class="language">
+            <a
+                href="{{ url()->current() . '?lang=nl' }}"
+                aria-current="{{ App::isLocale('nl') ? 'page' : 'false' }}"
+            >NL</a>
+            <a
+                href="{{ url()->current() . '?lang=en' }}"
+                aria-current="{{ App::isLocale('en') ? 'page' : 'false' }}"
+            >EN</a>
+        </div>
     </div>
 </header>
