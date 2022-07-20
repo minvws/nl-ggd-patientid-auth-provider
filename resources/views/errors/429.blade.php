0de9462a13772d88aea608ad365eb5e44e5e2d04
Joshua Thijssen <jthijssen@noxlogic.nl>
2022-07-20 10:41:31 +0200

    Added initial ratelimit error page

---
+++ resources/views/errors/429.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <section>
+        <div>
+            <h1>@lang('ratelimit.header')</h1>
+            <p>@lang('ratelimit.content')</p>
+        </div>
+    </section>
+
+@endsection
