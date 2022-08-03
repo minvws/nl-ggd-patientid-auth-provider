b7b2fa07eb535cf2537b8174ee160d4ba4f8bf52
Rein van der Vegt <rein@detesters.nl>
2022-08-03 11:30:45 +0200

    final!!

--- tests/robot_framework/lib/jwt_library.py
+++ tests/robot_framework/lib/jwt_library.py
@@ -5,11 +5,8 @@ import jwt
 class jwt_library():
 
     def decode_jwt_token(self, jwt_token):
-        # cert_str = str.encode(open("../../../secrets/cert.pem", "r").read())
         cert_str = str.encode(open("secrets/cert.pem", "r").read())
-
         cert_obj = load_pem_x509_certificate(cert_str, default_backend())
         public_key = cert_obj.public_key()
-
         decoded = jwt.decode(jwt_token, public_key, algorithms=["RS256"], audience="https://localhost:888/coronacheck/1.0")
         return decoded
